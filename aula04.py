9a7cb1c05984451e685791c30f84bb5eec131291
Devbox17 <devboxever17@gmail.com>
2019-12-24 09:49:32 -0300

    Atualizado

---
+++ aula04.py
@@ -0,0 +1,37 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7+4')
+print('7'+'4')
+print()
+
+nome = input('Qual é o seu nome?: ')
+idade = input('Qual é a sua idade?: ')
+peso = input('Qual é a seu peso?: ')
+
+print()
+print(nome, idade, peso)
+
+# Desafio 01
+
+print()
+nome = input('Qual é o seu nome?: ')
+
+print('Olá', nome, 'Prazer em te conhecer!')
+
+# Desafio 02
+
+print()
+dia = input('Informe o Dia: ')
+mes = input('Informe o mês: ')
+ano = input('Informe o ano: ')
+
+print()
+print(dia, '/', mes, '/', ano)
+
+# Desafio 03
+
+print()
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe outro número: '))
+soma = n1 + n2
+print(soma)
